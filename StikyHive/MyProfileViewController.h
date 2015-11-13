e7b0e5cb6c888f4978291dab582ad540cb593f38
Echo19900318 <zyj3182010@gmail.com>
2015-11-13 17:17:32 +0800

    AFNetWorking

---
+++ StikyHive/MyProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyProfileViewController.h
+//  StikyHive
+//
+//  Created by THV1WP15S on 13/11/15.
+//  Copyright (c) 2015 Stiky Hive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyProfileViewController : UIViewController <UIScrollViewDelegate,UIWebViewDelegate>
+
+
+
+@property (strong, nonatomic) IBOutlet UIScrollView *contentScrollView;
+
+@end
