ad1864ac20b64982cd5dd7e72a78f7da4f52f522
Nelson <tomneo2004@gmail.com>
2015-11-25 17:46:51 +0800

    2015/11/25 - before switch to master and do revert

---
+++ StikyHive/PrepareRequestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrepareRequestTableViewController.h
+//  StikyHive
+//
+//  Created by User on 25/11/15.
+//  Copyright © 2015 Stiky Hive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrepareRequestTableViewController : UITableViewController
+
+@end
---
+++ StikyHive/PrepareRequestViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrepareRequestViewController.h
+//  StikyHive
+//
+//  Created by User on 25/11/15.
+//  Copyright © 2015 Stiky Hive. All rights reserved.
+//
+
+#import "CropPhotoViewController.h"
+
+@interface PrepareRequestViewController : CropPhotoViewController
+
+@end
