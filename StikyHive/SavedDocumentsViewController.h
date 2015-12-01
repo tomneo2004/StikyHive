fbe0544b292e469b4cb7912afc133637f0f93917
Nelson <tomneo2004@gmail.com>
2015-12-01 17:15:57 +0800

    2015/12/1 - SavedDocuments controller, before update from master

---
+++ StikyHive/SavedDocumentsViewController.h
@@ -0,0 +1,14 @@
+//
+//  SavedDocumentsViewController.h
+//  StikyHive
+//
+//  Created by User on 1/12/15.
+//  Copyright © 2015 Stiky Hive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TitleViewController.h"
+
+@interface SavedDocumentsViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@end
