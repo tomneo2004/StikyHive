1f2039025308f88e1c4f242b9262928cc288eae0
Nelson <tomneo2004@gmail.com>
2015-12-01 18:07:55 +0800

    2015/12/1 - WIP

--- StikyHive/SavedDocumentsViewController.m
+++ StikyHive/SavedDocumentsViewController.m
@@ -7,21 +7,112 @@
 //
 
 #import "SavedDocumentsViewController.h"
+#import "WebDataInterface.h"
+#import "LocalDataInterface.h"
+#import "UIView+RNActivityView.h"
+#import "DocumentInfo.h"
 
 @interface SavedDocumentsViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 @end
 
-@implementation SavedDocumentsViewController
+@implementation SavedDocumentsViewController{
+    
+    NSMutableArray *_documentInfos;
+}
+
+@synthesize tableView = _tableView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (void)viewDidAppear:(BOOL)animated{
+    
+    [super viewDidAppear:animated];
+    
+    [self pullData];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Internal
+- (void)pullData{
+    
+    [self.view showActivityViewWithLabel:@"Refreshing..." detailLabel:@"Fetching data"];
+    [WebDataInterface getSavedDocument:[LocalDataInterface retrieveStkid] completion:^(NSObject *obj, NSError *error){
+    
+        dispatch_async(dispatch_get_main_queue(), ^{
+        
+            if(error != nil){
+            
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Unable to get data!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                [alert show];
+                
+                
+            }
+            else{
+                
+                NSDictionary *dic = (NSDictionary *)obj;
+                
+                if(((NSArray *)dic[@"documents"]).count <=0){
+                
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No result" message:@"No documents yet!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alert show];
+                    
+                }
+                else{
+                    
+                    _documentInfos = [[NSMutableArray alloc] init];
+                    
+                    for(NSDictionary *data in dic[@"documents"]){
+                        
+                        DocumentInfo *info = [DocumentInfo createDocumentInfoFromDictionary:data];
+                        [_documentInfos addObject:info];
+                    }
+                    
+                    [_tableView reloadData];
+                    
+                    [self.view hideActivityView];
+                    
+                    return;
+                }
+                
+            }
+            
+            [self.view hideActivityView];
+            
+            [self.navigationController popViewControllerAnimated:YES];
+            
+        });
+    }];
+}
+
+#pragma mark - UITableViewDataSource delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    if(_documentInfos != nil){
+        
+        return _documentInfos.count;
+    }
+    
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return nil;
 }
 
 /*
