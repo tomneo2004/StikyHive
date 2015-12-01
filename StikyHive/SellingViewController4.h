96989922574495a99e3a671fd25f109ff1753a1f
Echo19900318 <zyj3182010@gmail.com>
2015-12-01 15:15:48 +0800

    1201 --- update

--- StikyHive/SellingViewController4.h
+++ StikyHive/SellingViewController4.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SellingViewController4 : UIViewController <UIScrollViewDelegate>
+@interface SellingViewController4 : UIViewController <UIScrollViewDelegate, UIAlertViewDelegate>
 
 
 @property (strong, nonatomic) IBOutlet UIScrollView *contentScrollView;
--- StikyHive/SellingViewController4.m
+++ StikyHive/SellingViewController4.m
@@ -35,15 +35,63 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (alertView.tag == 201)
+    {
+        if (buttonIndex == 0)
+        {
+            NSLog(@"add latter");
+            
+            
+        }
+        else
+        {
+            NSLog(@"add now");
+        }
+    }
+    else if (alertView.tag == 202)
+    {
+        if (buttonIndex == 0)
+        {
+            NSLog(@"cancel");
+            [_switchBtn2 setOn:YES animated:YES];
+        }
+        else
+        {
+            NSLog(@"yes");
+        }
+    }
+    else if (alertView.tag == 301)
+    {
+        if (buttonIndex == 0)
+        {
+            NSLog(@"add latter");
+            
+            
+        }
+        else
+        {
+            NSLog(@"add now");
+        }
+    }
+    else if (alertView.tag == 302)
+    {
+        if (buttonIndex == 0)
+        {
+            NSLog(@"cancel");
+            [_switchBtn3 setOn:YES animated:YES];
+        }
+        else
+        {
+            NSLog(@"yes");
+        }
+    }
+
+
 }
-*/
+
 
 - (IBAction)switchBtn2Pressed:(id)sender
 {
@@ -53,18 +101,60 @@
         
         UIAlertView *btn2AlertOn = [[UIAlertView alloc] initWithTitle:@"Add 4 Photos" message:@"Do you want to add 4 Photos now?" delegate:self cancelButtonTitle:@"Add Latter" otherButtonTitles:@"Yes,Now", nil];
         
+        btn2AlertOn.tag = 201;
+        [btn2AlertOn show];
     }
     else
     {
         NSLog(@"toggle off");
+        
+        UIAlertView *btn2AlertOff = [[UIAlertView alloc] initWithTitle:@"Remove 4 Photos" message:@"Do you want to Remove 4 Photos now?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Yes", nil];
+        
+        btn2AlertOff.tag = 202;
+        [btn2AlertOff show];
+        
     }
     
     
 }
 
-- (IBAction)switchBtn3Pressed:(id)sender {
+- (IBAction)switchBtn3Pressed:(id)sender
+{
+    if ([_switchBtn3 isOn])
+    {
+        NSLog(@"toggle on");
+        
+        UIAlertView *btn3AlertOn = [[UIAlertView alloc] initWithTitle:@"Add a Video" message:@"Do you want to add a Video now?" delegate:self cancelButtonTitle:@"Add Latter" otherButtonTitles:@"Yes,Now", nil];
+        
+        btn3AlertOn.tag = 301;
+        [btn3AlertOn show];
+
+    }
+    else
+    {
+        NSLog(@"toggle off");
+        
+        UIAlertView *btn3AlertOff = [[UIAlertView alloc] initWithTitle:@"Remove a Video" message:@"Do you want to Remove a Video now?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Yes", nil];
+        
+        btn3AlertOff.tag = 302;
+        [btn3AlertOff show];
+
+    }
 }
 
-- (IBAction)switchBtn4Pressed:(id)sender {
+- (IBAction)switchBtn4Pressed:(id)sender
+{
+//    if ([_switchBtn4 isOn])
+//    {
+//        NSLog(@"toggle on");
+//        
+//        UIAlertView *btn4AlertOn = [[UIAlertView alloc] initWithTitle:@"Add a Video" message:@"Do you want to add a Video now?" delegate:self cancelButtonTitle:@"Add Latter" otherButtonTitles:@"Yes,Now", nil];
+//        
+//        btn4AlertOn.tag = 301;
+//        [btn4AlertOn show];
+//
+//    }
+    
+    
 }
 @end
