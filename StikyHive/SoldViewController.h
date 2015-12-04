c65ac0628b6de7d4b35e7894e57c9cab100d9aaa
Nelson <tomneo2004@gmail.com>
2015-12-04 16:56:33 +0800

    2015/12/4 - Sold, before update from master

---
+++ StikyHive/SoldViewController.h
@@ -0,0 +1,13 @@
+//
+//  SoldViewController.h
+//  StikyHive
+//
+//  Created by User on 4/12/15.
+//  Copyright © 2015 Stiky Hive. All rights reserved.
+//
+
+#import "TitleViewController.h"
+
+@interface SoldViewController : TitleViewController<UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ StikyHive/SoldViewController.m
@@ -0,0 +1,57 @@
+//
+//  SoldViewController.m
+//  StikyHive
+//
+//  Created by User on 4/12/15.
+//  Copyright © 2015 Stiky Hive. All rights reserved.
+//
+
+#import "SoldViewController.h"
+
+@interface SoldViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation SoldViewController
+
+@synthesize tableView = _tableView;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDataSource delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return nil;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
