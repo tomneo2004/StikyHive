77a0435c282d77e70646c8a5e23d5d78c86a1003
Nelson <tomneo2004@gmail.com>
2016-03-23 10:06:02 +0800

    2016/3/23 -Before merge

---
+++ StikyHive/SearchCell.m
@@ -0,0 +1,23 @@
+//
+//  SearchCell.m
+//  StikyHive
+//
+//  Created by User on 23/3/16.
+//  Copyright © 2016 Stiky Hive. All rights reserved.
+//
+
+#import "SearchCell.h"
+
+@implementation SearchCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ StikyHive/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  StikyHive
+//
+//  Created by User on 23/3/16.
+//  Copyright © 2016 Stiky Hive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
